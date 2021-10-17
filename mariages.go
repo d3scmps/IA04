ab6ee9986f98ab374d8cf1e9c6ad78adaea3cede
vilecler <67290929+vilecler@users.noreply.github.com>
2021-10-17 20:15:15 +0200

    Update mariages.go

--- mariages.go
+++ mariages.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 )
-
 type Mariages map[AgentID]AgentID
 
 func (m Mariages) Debug(){
@@ -13,18 +12,24 @@ func (m Mariages) Debug(){
 }
 
 func (m Mariages) IsStable(proposants []Agent, disposants []Agent) bool{
-  for proposantID, disposantID := range m{
-    for proposantID2, disposantID2 := range m{
-      proposant := GetAgent(proposants, proposantID)
-      disposant := GetAgent(disposants, disposantID)
-      proposant2 := GetAgent(proposants, proposantID2)
-      disposant2 := GetAgent(disposants, disposantID2)
+  for proposantID := range m{
+    for proposantID2  := range m{
+      disposantID := m[proposantID]
+      disposantID2 := m[proposantID2]
 
-      if proposant.Prefers(disposant2, disposant) && disposant2.Prefers(proposant, proposant2){ //le mariage n'est pas stable
-        return false
-      }
-      if disposant.Prefers(proposant2, proposant) && proposant2.Prefers(disposant, disposant2){ //le mariage n'est pas stable
-        return false
+      if(proposantID != proposantID2 && disposantID != disposantID2){
+
+        proposant := GetAgent(proposants, proposantID)
+        disposant := GetAgent(disposants, disposantID)
+        proposant2 := GetAgent(proposants, proposantID2)
+        disposant2 := GetAgent(disposants, disposantID2)
+
+        if proposant.Prefers(disposant2, disposant) && disposant2.Prefers(proposant, proposant2){ //le mariage n'est pas stable
+          return false
+        }
+        if disposant.Prefers(proposant2, proposant) && proposant2.Prefers(disposant, disposant2){ //le mariage n'est pas stable
+          return false
+        }
       }
     }
   }
